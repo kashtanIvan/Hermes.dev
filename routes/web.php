104b32f1fa7bc6bfe5b91d4f9c20fa420e0a67cb
Kashtan <kashtanivan@yandex.ru>
2017-01-23 22:22:30 +0200

    rewrote route: login and logout

--- routes/web.php
+++ routes/web.php
@@ -23,15 +23,15 @@ Route::group(['prefix' => 'categories'], function () {
 
 Route::group(['middleware' => 'login'], function () {
     Route::resource('product', 'ProductController');
-    Route::get('logout', 'Auth\LoginController@logout');
-    Route::get('profile', 'ProfileController@index');
+    Route::get('logout', ['as' => 'logout', 'Auth\LoginController@logout']);
+    Route::get('profile', ['as' => 'profile','ProfileController@index']);
 
 });
 Route::get('register', ['as' => 'register', 'uses' => 'Auth\RegisterController@showRegisterForm']);
 Route::post('register', ['as' => 'register', 'uses' => 'Auth\RegisterController@createUser']);
 
-Route::get('login', ['as' => 'register', 'uses' => 'Auth\LoginController@showLoginForm']);
-Route::post('login', ['as' => 'register', 'uses' => 'Auth\LoginController@loginUser']);
+Route::get('login', ['as' => 'login', 'uses' => 'Auth\LoginController@showLoginForm']);
+Route::post('login', ['as' => 'login', 'uses' => 'Auth\LoginController@loginUser']);
 
 Route::get('password/reset', ['as' => 'password/reset', 'uses' => 'Auth\ForgotPasswordController@index']);
 Route::post('password/email', ['as' => 'password/email', 'uses' => 'Auth\ForgotPasswordController@sendPasswordEmail']);
