56b2d285b62c58c70b45738f1478b8443be4ae08
Kashtan <kashtanivan@yandex.ru>
2017-02-13 22:01:19 +0200

    create OrderService

---
+++ app/Services/OrderService.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Cache;
+
+class OrderService
+{
+
+
+
+    public function __construct()
+    {
+
+    }
+
+
+
+}
