25c416d7ee99dd4b61172a81f806f11dfccd60f2
Kashtan <kashtanivan@yandex.ru>
2017-02-18 11:24:24 +0200

    create migration menu_items table

---
+++ database/migrations/2017_02_18_091211_create_menu_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenuItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50)->default('/');
+            $table->string('slug', 50)->default('/');
+            $table->integer('menu_id')->unsigned()->default(0);
+            $table->integer('order_id')->unsigned()->default(0);
+            $table->boolean('hidden')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu_items');
+    }
+}
