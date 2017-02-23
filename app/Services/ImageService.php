9714b252e311d45fcb78007544e9c8cf70036e07
Kashtan <kashtanivan@yandex.ru>
2017-02-23 22:37:21 +0200

    add validator image

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -4,8 +4,11 @@
 namespace App\Services;
 
 use App\Image;
+use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Cache;
+use Symfony\Component\HttpFoundation\Request;
+use Intervention\Image\Facades\Image as Imagick;
 
 class ImageService
 {
@@ -22,6 +25,22 @@ class ImageService
         return 'Pong';
     }
 
+    public function addImage($data){
+        $file = $data->file()['image'];
+        $imagick = Imagick::make($file);
+//        dd($file, $imagick);
+        $postImage = [
+            'ext' => $file->getClientOriginalExtension(),
+            'width' => $imagick->width(),
+            'height' => $imagick->height(),
+            'size' => $imagick->filesize(),
+        ];
+//        dd($postImage);
+        $image = new Image();
+        $validator = Validator::make($postImage, $image->rules);
+        dd($validator->errors()->all());
+    }
+
     public function getAnyImage()
     {
         $image = Image::all()->random(1); //->toArray();
@@ -32,7 +51,7 @@ class ImageService
         $image = "{$root}{$image->location}{$image->id}.{$image->ext}";
 
         $img = asset($image);
-        //dd($image);
+        dd($image);
         //echo $imageFile;
         echo "<img src =" . $img . ">";
         exit();
