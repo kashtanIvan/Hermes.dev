594815654e1b756fa557114ca4713b783b24d9b7
Kashtan <kashtanivan@yandex.ru>
2017-02-23 22:42:50 +0200

    add CheckoutService

--- app/Services/CheckoutService.php
+++ app/Services/CheckoutService.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 
 class CheckoutService
 {
+
+    private $_itemsKey = 'checkout.items';
+    private $_sumKey = 'checkout.sum';
 //    private $user;
     private $_reugest;
 
@@ -22,13 +25,12 @@ class CheckoutService
     public function setItems($data = [])
     {
         $this->clear();
-        session(['checkout' => $data]);
+        session([$this->_itemsKey => $data]);
         $ids = $this->getItemsIds();
-        //var_dump($ids->toArray());
-        //exit();
         $price = (new Item())->whereIn('id', $ids)->get()->sum('price');
-        dd($price);
         $this->setSum($price);
+//        dd($data);
+        $this->increase($idProduct = 1);
     }
 
 
@@ -44,21 +46,25 @@ class CheckoutService
 
     }
 
+    public function getItemById($id){
+        return collect($this->getItems())->where('id', $id)->first();
+    }
+
 
     public function getSum()
     {
-        return session('checkout.items');
+        return session($this->_sumKey);
     }
 
 
     public function setSum($sum)
     {
-        session(['checkout.sum' => $sum]);
+        session([$this->_sumKey => $sum]);
     }
 
     public function getItems()
     {
-        $data = session('checkout');
+        $data = session($this->_itemsKey);
         return $data;
     }
 
@@ -70,6 +76,11 @@ class CheckoutService
 
     public function increase($id)
     {
+        $item = $this->getItemById($id);
+     //    $item->qty++;
+        //$this->update();
+            $item['qty']++;
+        dd($item);
 
     }
 
