f37db848f944c86ef913e9155b8b857eabc38d30
Kashtan <kashtanivan@yandex.ru>
2017-03-06 22:55:43 +0200

    create model ImageProduct

---
+++ app/ImageProduct.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ImageProduct extends Model
+{
+    protected $table = 'image_products';
+
+    protected $fillable = [
+        'image_id',
+        'prod_id',
+    ];
+
+    public function product()
+    {
+        return $this->hasOne('App\Product', 'image_id');
+    }
+}
