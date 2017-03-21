a428c8f23fdfa4804ed1d3bc833120c795e2c48e
Kashtan <kashtanivan@yandex.ru>
2017-03-21 20:20:45 +0200

    Added stylistics

--- resources/views/shop/cabinet.blade.php
+++ resources/views/shop/cabinet.blade.php
@@ -3,20 +3,26 @@
 
 @section('content')
 
-    <div>{{ $title }}</div>
-    <ul>
-        <li><a href="{{ route('product.index') }}">Товары</a> </li>
-        <li><a href="{{ route('product.create') }}">Добавить товар</a> </li>
-    </ul>
-    Profile dashboard <br>
-    Email: {{$user->email}}
+    <div class="container">
+        {{--<div class="row">--}}
+            <p><h1> {{ $title }} </h1></p>
+        {{--</div>--}}
+        <div class="row">
+            <ul class="nav navbar-nav">
+                <li><a href="{{ route('product.index') }}">Товары</a></li>
+                <li><a href="{{ route('product.create') }}">Добавить товар</a></li>
+            </ul>
+        </div>
+        Profile dashboard <br>
+        Email: {{$user->email}}
 
-    <br>
+        <br>
 
-    {{ Form::open(array('url' => 'password/email', 'method' => 'post')) }}
+        {{ Form::open(array('url' => 'password/email', 'method' => 'post')) }}
 
-    {{ Form::submit('change password') }}
-    {{ Form::close() }}
+        {{ Form::submit('change password') }}
+        {{ Form::close() }}
 
+    </div>
 
 @endsection
