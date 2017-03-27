9e51c551b56ab487c06f345dfb983df9ea377a70
Kashtan <kashtanivan@yandex.ru>
2017-03-27 22:51:07 +0300

    fixed controller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -11,6 +11,18 @@ class ProfileController extends Controller
     public function index()
     {
         $user = Sentinel::check();
-        return view('shop.cabinet')->with(['user'=>$user]);
+        return view('shop.cabinet')->with([
+            'title' => 'profile',
+            'user' => $user
+        ]);
     }
+
+    public function indexCabinet()
+    {
+        $title = 'Cabinet';
+//        $brand = $this->_productServices->getBrand();
+        $user = Sentinel::check();
+        return view('shop.cabinet')->with(['user' => $user, 'title' => $title]); //->with('brand' , $brand);
+    }
+
 }
